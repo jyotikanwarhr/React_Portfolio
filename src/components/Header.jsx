52c6255d6f70ed8f5409b0b4500b8060d178d7b9
jyotikanwarhr <hr.jyotibk@gmail.com>
2025-04-08 22:33:08 -0700

    Updated About Me page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return (
         <header class="bg-gray-100 py-4">
         <nav class="container mx-auto flex justify-between items-center">
-          <div class="logo">Your Name</div>
+          <div class="logo">Jyoti Kanwar</div>
           <ul class="flex items-center">
             <li class="mr-6"><a href="/" class="text-gray-700 hover:text-gray-900">About</a></li>
             <li class="mr-6"><a href="/portfolio" class="text-gray-700 hover:text-gray-900">Portfolio</a></li>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,16 @@
 function About() {
     return (
         <div>
-            <h1>This is the About Page</h1>
+            <h1>About Me</h1>
+            <div>
+            <img src="https://via.placeholder.com/150" alt="My Picture" />
+            </div>
+            <div>
+            <p> My name is Jyoti. I am Recruiter for the past 10 years but I am looking for a transition into IT. I have completed the Bootcamp in Software Engineering.</p>
+            </div>
         </div>
+        
+
     )
 }
 
